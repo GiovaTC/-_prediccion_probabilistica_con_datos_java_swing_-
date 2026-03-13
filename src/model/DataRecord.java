62dbbde91e055da632c15ebe37f267f3df118308
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-13 10:15:47 -0500

    commit - DataRecord / .

--- src/model/DataRecord.java
+++ src/model/DataRecord.java
@@ -1,4 +1,29 @@
 package model;
 
 public class DataRecord {
+
+    private String category;
+    private int count;
+    private double probability;
+
+    public DataRecord(String category, int count) {
+        this.category = category;
+        this.count = count;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }   
 }
